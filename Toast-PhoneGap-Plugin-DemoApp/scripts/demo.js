8c822c48c2a92511d3283271853a28395614bf6a
Eddy Verbruggen <eddyverbruggen@gmail.com>
2014-04-29 10:13:29 -0500

    Show a message when this plugin is used in the simulator

--- Toast-PhoneGap-Plugin-DemoApp/scripts/demo.js
+++ Toast-PhoneGap-Plugin-DemoApp/scripts/demo.js
@@ -6,28 +6,48 @@
 
         // short lasting messages
         showToast_shortTop: function () {
-            window.plugins.toast.showShortTop('Your changes have been saved', this.onSuccess, this.onError);
+            if (!this.checkSimulator()) {
+	            window.plugins.toast.showShortTop('Your changes have been saved', this.onSuccess, this.onError);
+            }
         },
         
         showToast_shortCenter: function () {
-            window.plugins.toast.showShortCenter('There were validation errors', this.onSuccess, this.onError);
+            if (!this.checkSimulator()) {
+	            window.plugins.toast.showShortCenter('There were validation errors', this.onSuccess, this.onError);
+            }
         },
 
         showToast_shortBottom: function () {
-            window.plugins.toast.showShortBottom('Account created successfully :)', this.onSuccess, this.onError);
+            if (!this.checkSimulator()) {
+	            window.plugins.toast.showShortBottom('Account created successfully :)', this.onSuccess, this.onError);
+            }
         },
 
         // longer lasting messages
         showToast_longTop: function () {
-            window.plugins.toast.showLongTop('The battery is almost out of juice!', this.onSuccess, this.onError);
+            if (!this.checkSimulator()) {
+	            window.plugins.toast.showLongTop('The battery is almost out of juice!', this.onSuccess, this.onError);
+            }
         },
         
         showToast_longCenter: function () {
-            window.plugins.toast.showLongCenter('Connection failure, please try again later', this.onSuccess, this.onError);
+            if (!this.checkSimulator()) {
+	            window.plugins.toast.showLongCenter('Connection failure, please try again later', this.onSuccess, this.onError);
+            }
         },
 
         showToast_longBottom: function () {
-            window.plugins.toast.showLongBottom('Please turn on WiFi so we can do a better job determining your location', this.onSuccess, this.onError);
+            if (!this.checkSimulator()) {
+                window.plugins.toast.showLongBottom('Please turn on WiFi so we can do a better job determining your location', this.onSuccess, this.onError);
+            }
+        },
+
+        checkSimulator: function() {
+            if (window.plugins === undefined) {
+                alert('Plugin not available. Are you running in the simulator?');
+                return true;
+            }
+            return false;
         },
 
         // callbacks
