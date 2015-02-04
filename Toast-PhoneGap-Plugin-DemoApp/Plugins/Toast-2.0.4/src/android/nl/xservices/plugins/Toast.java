2a8bb117ddcc98ed4f19d96e499bebcd33057c92
Eddy Verbruggen <eddyverbruggen@gmail.com>
2015-02-04 13:21:31 +0000

    plugin updated to 2.0.4

--- Toast-PhoneGap-Plugin-DemoApp/Plugins/Toast-2.0.4/src/android/nl/xservices/plugins/Toast.java
+++ Toast-PhoneGap-Plugin-DemoApp/Plugins/Toast-2.0.4/src/android/nl/xservices/plugins/Toast.java
@@ -6,13 +6,32 @@ import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
 
+/*
+    // TODO nice way for the Toast plugin to offer a longer delay than the default short and long options
+    // TODO also look at https://github.com/JohnPersano/Supertoasts
+    new CountDownTimer(6000, 1000) {
+      public void onTick(long millisUntilFinished) {toast.show();}
+      public void onFinish() {toast.show();}
+    }.start();
+
+    Also, check https://github.com/JohnPersano/SuperToasts
+ */
 public class Toast extends CordovaPlugin {
 
   private static final String ACTION_SHOW_EVENT = "show";
 
+  private android.widget.Toast mostRecentToast;
+
+  // note that webView.isPaused() is not Xwalk compatible, so tracking it poor-man style
+  private boolean isPaused;
+
   @Override
   public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
     if (ACTION_SHOW_EVENT.equals(action)) {
+
+      if (this.isPaused) {
+        return true;
+      }
 
       final String message = args.getString(0);
       final String duration = args.getString(1);
@@ -43,6 +62,7 @@ public class Toast extends CordovaPlugin {
           }
 
           toast.show();
+          mostRecentToast = toast;
           callbackContext.success();
         }
       });
@@ -52,5 +72,18 @@ public class Toast extends CordovaPlugin {
       callbackContext.error("toast." + action + " is not a supported function. Did you mean '" + ACTION_SHOW_EVENT + "'?");
       return false;
     }
+  }
+
+  @Override
+  public void onPause(boolean multitasking) {
+    if (mostRecentToast != null) {
+      mostRecentToast.cancel();
+    }
+    this.isPaused = true;
+  }
+
+  @Override
+  public void onResume(boolean multitasking) {
+    this.isPaused = false;
   }
 }
